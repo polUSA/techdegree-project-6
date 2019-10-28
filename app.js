428ed6ed8578a48a67d45262115a67c9ae824b8c
Stefano <stefanonoventa@gmail.com>
2019-10-28 05:44:44 +0400

    almost everything working

--- app.js
+++ app.js
@@ -6,10 +6,15 @@ const data = require('./data.json')
 //initialize express app
 const app = express()
 
+//setup of the view engine 
 app.set('view engine', 'pug')
 
 const staticPath = path.join(__dirname, 'public')
 app.use('/static', express.static(staticPath))
+
+//static file server to serve the images
+const staticPath2 = path.join(__dirname, 'images')
+app.use('/images', express.static(staticPath2))
 
 app.get('/',(req, res, next)=>{
   res.render('index', { projects: data.projects })
@@ -21,23 +26,32 @@ app.get('/about',(req, res, next)=>{
 
 //used a regular expression for the route /project/id so it must be at at least a digit (doesn't accept other parameters)
 app.get(/^\/project\/(\d+)$/, (req, res, next) => {
-  res.send(`you requested the project ${req.params[0]}`)
+  //I removed 1 from the paramter so it matches the projects array index
+  const id = req.params[0] - 1
+  res.render('project', { project: data.projects[id] })
+
 })
 
 //Error handler miiddlware
 
-//404 error handler
+//404
 app.use((req, res, next)=>{
-  console.log('404 Not Found error triggered')
   const error = new Error('404 Page Not Found')
   error.status = 404
   next(error)
 })
 
+//error handling middleware
 app.use((err, req, res, next)=>{
-  console.error(err)
-  debugger
-  res.status(err.status).send(`${err.status}`)
+  const message = err.message
+  const errCode = err.status
+  console.error(`An error occured and it's been handled. \n${message}, \nError code: ${errCode}`)
+  next(err)
+})
+
+//the error object is passed from the the midlleware that called the error handling with next(someErrorObject)
+app.use((err, req, res, next)=>{
+  res.status(err.status).render('error', { err })
 })
 
 
